5ada432105ff46f73fcffb3f746d1c8a576f5f62
“oxchen12” <emailoxc@gmail.com>
2021-01-14 19:22:53 -0500

    initial

---
+++ game.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def main():
+    # 1 represents player 1, -1 represents player 2, 0 is neutral
+    board = np.array(
+        [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]
+         ]
+    )
+    turn = 1
+    win = 0
+    while win == 0:
+        pass
+
+
+if __name__ == "__main__":
+    pass
